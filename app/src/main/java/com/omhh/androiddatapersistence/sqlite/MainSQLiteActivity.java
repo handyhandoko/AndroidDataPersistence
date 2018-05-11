d4c2666ae9ddfedc8c9e2b307c502caaa2971001
Handy Handoko <handy.handoko@digitalnusantarastudio.com>
2018-05-11 13:10:06 +0800

    add data to sqlite

--- app/src/main/java/com/omhh/androiddatapersistence/sqlite/MainSQLiteActivity.java
+++ app/src/main/java/com/omhh/androiddatapersistence/sqlite/MainSQLiteActivity.java
@@ -1,15 +1,23 @@
 package com.omhh.androiddatapersistence.sqlite;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.EditText;
 
 import com.omhh.androiddatapersistence.R;
+import com.omhh.androiddatapersistence.sqlite.data.MessagesContract;
+import com.omhh.androiddatapersistence.sqlite.data.MessagesOpenHelpers;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainSQLiteActivity extends AppCompatActivity {
+    final MessagesOpenHelpers dbHelpers = new MessagesOpenHelpers(this, MessagesContract.MessagesEntry.TABLE_NAME);
+    @BindView(R.id.edtMessage) EditText edtMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +25,11 @@ public class MainSQLiteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_sqlite);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.btnSend)
+    public void save(){
+        dbHelpers.insert(edtMessage.getText().toString());
+    }
 }
--- app/src/main/java/com/omhh/androiddatapersistence/sqlite/data/MessagesOpenHelpers.java
+++ app/src/main/java/com/omhh/androiddatapersistence/sqlite/data/MessagesOpenHelpers.java
@@ -1,5 +1,6 @@
 package com.omhh.androiddatapersistence.sqlite.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,7 +9,7 @@ public class MessagesOpenHelpers extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "database.db";
     private static final Short DATABASE_VERSION = 1;
 
-    public MessagesOpenHelpers(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public MessagesOpenHelpers(Context context, String name) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
 
     }
@@ -28,5 +29,11 @@ public class MessagesOpenHelpers extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ MessagesContract.MessagesEntry.TABLE_NAME);
         onCreate(sqLiteDatabase);
+    }
+
+    public void insert(String message){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MessagesContract.MessagesEntry.COLLUMN_MESSAGE, message);
+        getWritableDatabase().insert(MessagesContract.MessagesEntry.TABLE_NAME, null, contentValues);
     }
 }
