2e1cf2f155e80d30ea6c74bbfc4db3bf8f8d7673
Handy Handoko <handy.handoko@digitalnusantarastudio.com>
2018-05-13 07:43:50 +0800

    tambah insert dan query

--- app/src/main/java/com/omhh/androiddatapersistence/sqlite/data/MessagesOpenHelpers.java
+++ app/src/main/java/com/omhh/androiddatapersistence/sqlite/data/MessagesOpenHelpers.java
@@ -2,8 +2,13 @@ package com.omhh.androiddatapersistence.sqlite.data;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MessagesOpenHelpers extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "database.db";
@@ -19,8 +24,8 @@ public class MessagesOpenHelpers extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL(
             "CREATE TABLE " +
             MessagesContract.MessagesEntry.TABLE_NAME + " (" +
-            MessagesContract.MessagesEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-            MessagesContract.MessagesEntry.COLLUMN_MESSAGE + "TEXT " +
+            MessagesContract.MessagesEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            MessagesContract.MessagesEntry.COLLUMN_MESSAGE + " TEXT " +
                 ");"
         );
     }
@@ -32,8 +37,31 @@ public class MessagesOpenHelpers extends SQLiteOpenHelper {
     }
 
     public void insert(String message){
+        SQLiteDatabase db = this.getWritableDatabase();
+
         ContentValues contentValues = new ContentValues();
         contentValues.put(MessagesContract.MessagesEntry.COLLUMN_MESSAGE, message);
-        getWritableDatabase().insert(MessagesContract.MessagesEntry.TABLE_NAME, null, contentValues);
+
+        db.insert(MessagesContract.MessagesEntry.TABLE_NAME, null, contentValues);
+    }
+
+    public List<String> query(@Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.query(
+            MessagesContract.MessagesEntry.TABLE_NAME,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        );
+
+        ArrayList<String> messages = new ArrayList<>();
+        while(cursor.moveToNext()){
+            messages.add(cursor.getString(cursor.getColumnIndex(MessagesContract.MessagesEntry.COLLUMN_MESSAGE)));
+        }
+        return messages;
     }
 }
